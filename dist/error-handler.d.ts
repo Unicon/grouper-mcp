cdb2e1b94f301d4a94edc45847785d5fcbf21863
Jim Beard <jbeard@unicon.net>
2025-08-15 13:55:30 -0700

    cleaning up from initial import

--- dist/error-handler.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class GrouperError extends Error {
-    statusCode?: number | undefined;
-    grouperCode?: string | undefined;
-    constructor(message: string, statusCode?: number | undefined, grouperCode?: string | undefined);
-}
-export declare function handleGrouperError(error: any): GrouperError;
-export declare function logError(error: GrouperError, context?: string): void;
-//# sourceMappingURL=error-handler.d.ts.map
